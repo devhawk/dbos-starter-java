ce327c04e2d2a4cd0d9d5c21f4d653ad56353219
Harry Pierson <harry.pierson@dbos.dev>
2026-03-12 14:09:03 -0700

    update build to emit test info

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,6 +20,23 @@ dependencies {
 
 java { toolchain { languageVersion = JavaLanguageVersion.of(21) } }
 
+tasks.test {
+  testLogging {
+    // Show all test events
+    events("passed", "skipped", "failed", "standardOut", "standardError")
+    
+    // Maximum detail for failures
+    exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+    showExceptions = true
+    showCauses = true
+    showStackTraces = true
+    showStandardStreams = true
+    
+    // Show detailed info
+    minGranularity = 0  // Show individual test methods
+  }
+}
+
 spotless {
   java {
     googleJavaFormat()
